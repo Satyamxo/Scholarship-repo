8d6fe6b88ba6f885b130883ece78083310d32177
Satyam Gupta <guptas62434@gmail.com>
2025-05-18 23:40:38 +0530

    Again deploy changes

---
+++ front/src/redux/store.js
@@ -0,0 +1,12 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import counterSlice from "../redux/States/stateSclice.js"
+
+
+// // Configure the Redux store
+// const store = configureStore({
+//   reducer: {
+//    backendUrl: backendReducer
+//   },
+// });
+
+// export default store;
